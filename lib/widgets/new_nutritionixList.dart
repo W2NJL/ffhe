ec03bc16d800a88eee20d1bb40708450a82d0c20
W2NJL <55940428+W2NJL@users.noreply.github.com>
2021-04-07 02:33:19 -0400

    Added dupe check for newnutritionlist

--- lib/widgets/new_nutritionixList.dart
+++ lib/widgets/new_nutritionixList.dart
@@ -164,9 +164,9 @@ class NewNutritionixListState extends State<NewNutritionixList> {
 // users[index]['picture']['large'],
 // ),
                 ),
-                title: Text((users[index]['fields']['item_name'])),
+                title: Text((users[index]['item_name'])),
                 subtitle: Text(
-                    (users[index]['fields']['nf_calories'].toString()) +
+                    (users[index]['nf_calories'].toString()) +
                         ' calories'),
               ),
             );
@@ -182,7 +182,7 @@ class NewNutritionixListState extends State<NewNutritionixList> {
     final ref = referenceDatabase.reference().child('User');
 
     return new AlertDialog(
-      title: Text(user['fields']['item_name']),
+      title: Text(user['item_name']),
       content: new Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -193,9 +193,9 @@ class NewNutritionixListState extends State<NewNutritionixList> {
       actions: <Widget>[
         new FlatButton(
           onPressed: () {
-            ref.child('DietVals').child(formattedDate).child('Calories').set(ServerValue.increment(user['fields']['nf_calories']));
-            ref.child('DietVals').child(formattedDate).child('Sodium').set(ServerValue.increment(user['fields']['nf_sodium']));
-            ref.child('DietVals').child(formattedDate).child('Meals').child(user['fields']['item_name']).set(ServerValue.increment(user['fields']['nf_calories']));
+            ref.child('DietVals').child(formattedDate).child('Calories').set(ServerValue.increment(user['nf_calories']));
+            ref.child('DietVals').child(formattedDate).child('Sodium').set(ServerValue.increment(user['nf_sodium']));
+            ref.child('DietVals').child(formattedDate).child('Meals').child(user['item_name']).set(ServerValue.increment(user['nf_calories']));
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(
@@ -213,11 +213,11 @@ class NewNutritionixListState extends State<NewNutritionixList> {
   static Widget _buildAboutText(user) {
     return new RichText(
       text: new TextSpan(
-        text: 'Calories: ' + user['fields']['nf_calories'].toString() + " calories" + '\n\n' +
-            'Total Carbohydrates: ' + user['fields']['nf_total_carbohydrate'].toString() + " g" + '\n\n' +
-            'Sodium: ' + user['fields']['nf_sodium'].toString() + " mg" + '\n\n' +
-            'Total Fat: '  + user['fields']['nf_total_fat'].toString() + " g" + '\n\n' +
-            'Cholesterol: ' + user['fields']['nf_cholesterol'].toString() + " mg" + '\n\n',
+        text: 'Calories: ' + user['nf_calories'].toString() + " calories" + '\n\n' +
+            'Total Carbohydrates: ' + user['nf_total_carbohydrate'].toString() + " g" + '\n\n' +
+            'Sodium: ' + user['nf_sodium'].toString() + " mg" + '\n\n' +
+            'Total Fat: '  + user['nf_total_fat'].toString() + " g" + '\n\n' +
+            'Cholesterol: ' + user['nf_cholesterol'].toString() + " mg" + '\n\n',
         style: const TextStyle(color: Colors.black87),
         // children: <TextSpan>[
         //   const TextSpan(text: 'The app was developed with '),
@@ -314,83 +314,104 @@ class NewNutritionixListState extends State<NewNutritionixList> {
         print("CarbSum is: " + carbSum.toString());
         }));
 
-      params = {
-        "appId": "816cee15",
-        "appKey": "aab0a0a4c4224eca770bf5a2a0f4c984",
-        "queries":{
-          "brand_name": restaurant,
-        },
-        "fields": [
-          "item_name",
-          "brand_name",
-          "nf_calories",
-          "nf_sodium",
-          "nf_total_fat",
-          "item_type",
-          "nf_cholesterol",
-          "nf_total_carbohydrate"
-        ],
-        "offset": page,
-        "limit": 50,
-        "sort": {
-          "field": "nf_calories",
-          "order": "desc"
-        },
-        "filters": {
+      List tList = new List();
 
-          "nf_calories": {
-            "from": 0,
-            "to": calorieSum
+      while(tList.length < 10) {
+        params = {
+          "appId": "816cee15",
+          "appKey": "aab0a0a4c4224eca770bf5a2a0f4c984",
+          "queries": {
+            "brand_name": restaurant,
           },
-          "nf_sodium": {
-            "from": 0,
-            "to": sodiumSum
+          "fields": [
+            "item_name",
+            "brand_name",
+            "nf_calories",
+            "nf_sodium",
+            "nf_total_fat",
+            "item_type",
+            "nf_cholesterol",
+            "nf_total_carbohydrate"
+          ],
+          "offset": page,
+          "limit": 50,
+          "sort": {
+            "field": "nf_calories",
+            "order": "desc"
           },
-          "nf_total_carbohydrate": {
-            "from": 0,
-            "to": carbSum
+          "filters": {
+
+            "nf_calories": {
+              "from": 0,
+              "to": calorieSum
+            },
+            "nf_sodium": {
+              "from": 0,
+              "to": sodiumSum
+            },
+            "nf_total_carbohydrate": {
+              "from": 0,
+              "to": carbSum
+            }
           }
-        }
-      };
+        };
 
 
+        //var url = 'https://api.nutritionix.com/v1_1/search/' + restaurant + '?results='+ index.toString() + ':' + (index+50).toString() + '&fields=item_name,brand_name,nf_calories,nf_sodium,nf_sugars,nf_cholesterol,nf_total_fat,nf_dietary_fiber&appId=816cee15&appKey=aab0a0a4c4224eca770bf5a2a0f4c984';
+        //print(url);
+        var url = 'https://api.nutritionix.com/v1_1/search';
+        try {
+          final response = await dio.post(url,
+            options: Options(headers: {
+              HttpHeaders.contentTypeHeader: "application/json",
+            }),
+            data: params,
+          );
 
 
-      //var url = 'https://api.nutritionix.com/v1_1/search/' + restaurant + '?results='+ index.toString() + ':' + (index+50).toString() + '&fields=item_name,brand_name,nf_calories,nf_sodium,nf_sugars,nf_cholesterol,nf_total_fat,nf_dietary_fiber&appId=816cee15&appKey=aab0a0a4c4224eca770bf5a2a0f4c984';
-      //print(url);
-      var url = 'https://api.nutritionix.com/v1_1/search';
-      try {
-        final response = await dio.post(url,
-          options: Options(headers: {
-            HttpHeaders.contentTypeHeader: "application/json",
-          }),
-          data: params,
-        );
+          List<String> foodList = <String>[];
 
-        List tList = new List();
 
-        for (int i = 0; i < response.data['hits'].length; i++) {
-          if (determineFood(response.data['hits'][i]['fields']['item_name']) ==
-              category) {
-            tList.add(response.data['hits'][i]);
+          for (int i = 0; i < response.data['hits'].length; i++) {
+            if (determineFood(
+                response.data['hits'][i]['fields']['item_name']) ==
+                category && !foodList.contains(
+                response.data['hits'][i]['fields']['item_name']
+                    .toString()
+                    .toLowerCase())) {
+              tList.add(response.data['hits'][i]['fields']);
+              foodList.add(response.data['hits'][i]['fields']['item_name']
+                  .toString()
+                  .toLowerCase());
+            }
+
           }
-          print(response.data['hits'][i]['fields']['item_name']);
-          print(determineFood(response.data['hits'][i]['fields']['item_name']));
-        }
 
-        setState(() {
-          isLoading = false;
-          users.addAll(tList);
-          page += 50;
-        });
-      }
-      catch (error, stackTrace) {
-        print("Exception occurred: $error  stackTrace: $stackTrace");
+
+
+          setState(() {
+            page += 50;
+          });
+
 
       }
+
+
+    catch (error, stackTrace) {
+    print("Exception occurred: $error  stackTrace: $stackTrace");
+
     }
 
   }
+      setState(() {
+        isLoading = false;
+        print(tList.toString());
+        users.addAll(tList);
+      });
+    }
+    }
+
+
 
   Widget _buildProgressIndicator() {
     return new Padding(
@@ -431,7 +452,7 @@ class NewNutritionixListState extends State<NewNutritionixList> {
     String categoryWord;
     const int categories = 3;
     final beverageReg =  RegExp("(?:coke|tea|dr. pepper|pepsi|sprite|orange juice|coffee|milk|root beer|latte|cola|mocha|americano|frappe|shake|milk)", caseSensitive: false);
-    final entreeReg =  RegExp("(?:salad|sandwich|soup|burger|pasta|gyro|hoagie|buffalo|sandwich|nuggets|filet|strips|pounder|tenders|mac|chicken|tacos|quesadilla|waffle|toast|pancake|omelette|omelet|sausage|breakfast|bagel|egg|muffin|hotcakes|steak|burrito|pie|fritter)", caseSensitive: false);
+    final entreeReg =  RegExp("(?:salad|bowl|sandwich|soup|burger|pasta|gyro|hoagie|buffalo|sandwich|nuggets|filet|strips|pounder|tenders|mac|chicken|tacos|quesadilla|waffle|toast|pancake|omelette|omelet|sausage|breakfast|bagel|egg|muffin|hotcakes|steak|burrito|pie|fritter)", caseSensitive: false);
 
     final sideReg =  RegExp("(?:dressing|mustard|jelly|peanuts|coleslaw|sauce|lemonade|fries|potato|corn|rice|balsamic|beans|blue cheese)", caseSensitive: false);
 
